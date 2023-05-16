470f70c7bedac3de86c652609ea041f1989ce7b2
Matthew Du Pont <1626189+mattdp@users.noreply.github.com>
2023-05-15 21:47:03 -0400

    2-2 completed, after removing complexity in reversing list

--- 2-code.py
+++ 2-code.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 
-translation = {
-    "A": "Rock",
-    "B": "Paper",
-    "C": "Scissors",
-    "X": "Rock",
-    "Y": "Paper",
-    "Z": "Scissors",    
-}
+#next: verbose output, so can figure out what's scoring wrong
+#could also make a mini input file to test
 
 points = {
     "Rock": 1,
     "Paper": 2,
     "Scissors": 3,
+    "Lose": 0,
+    "Draw": 3,
+    "Win": 6,
 }
 
 a_beats_b = {
@@ -21,28 +18,102 @@ a_beats_b = {
     "Scissors": "Paper",
 }
 
-points_draw = 3
-points_win = 6
+a_loses_b = {
+    "Rock": "Paper",
+    "Scissors": "Rock",
+    "Paper": "Scissors",
+}
 
-def part_one():
+#misread - this solves if first input is MY choice, but it's OPPONENT choice
+# def part_two_misread():
+#     f = open("./2-input.txt","r")
+#     text = f.read()
+
+#     scores = [ #16936, which is too high according to site
+#         points["Rock"]*text.count("A"), #267 * 1 = 267
+#         points["Paper"]*text.count("B"), #500 * 2 = 1000
+#         points["Scissors"]*text.count("C"), #1733 * 3 = 5199
+#         points["Draw"]*text.count("Y"), #834 * 3 = 2502
+#         points["Win"]*text.count("Z"), #1328 * 6 = 7968
+#     ]
+#     print(scores)
+#     return(sum(scores))
+
+def score_line_again(them, me, verbose=False):
+    translation = {
+    "A": "Rock",
+    "B": "Paper",
+    "C": "Scissors",
+    "X": "Lose",
+    "Y": "Draw",
+    "Z": "Win",    
+    }
+
+    their_choice = translation[them]
+
+    #Win/lose/draw points
+    total_points = points[translation[me]]
+    if(verbose):
+        print(f"***\nWLD: {total_points} for {translation[me]}")
+
+    if(translation[me] == "Draw"):
+        pts = points[their_choice]
+        if(verbose):
+            print(f"Thing: {pts} for {translation[them]}")
+        total_points += pts
+    elif(translation[me] == "Lose"):
+        selection = a_beats_b[their_choice]
+        pts = points[selection] 
+        if(verbose):
+            print(f"Thing: {selection} gives {pts} for LOSE to {translation[them]}")
+        total_points += pts
+    else:
+        selection = a_loses_b[their_choice]
+        pts = points[selection]
+        if(verbose):
+            print(f"Thing: {selection} gives {pts} for WIN to {translation[them]}")
+        total_points += pts
+    
+    if(verbose):
+        print(f"{translation[them]} {translation[me]}: {total_points} points")
+
+    return total_points
+
+def score_line(them, me):
+    translation = {
+    "A": "Rock",
+    "B": "Paper",
+    "C": "Scissors",
+    "X": "Rock",
+    "Y": "Paper",
+    "Z": "Scissors",    
+    }
+
+    total_points = points[translation[me]]
+
+    if(translation[me] == translation[them]):
+        total_points += points["Draw"]
+    elif(a_beats_b[translation[me]] == translation[them]):
+        total_points += points["Win"]
+    
+    return total_points
+
+#part_number: part 1 or part 2 of the question
+def frame(part_number = 1):
     cumulative_points = 0
 
     f = open("./2-input.txt","r")
     for line in f:
         try:
-            cumulative_points += score_line(line[0],line[2])
+            if part_number == 1:
+                cumulative_points += score_line(line[0],line[2])
+            else:
+                cumulative_points += score_line_again(line[0],line[2],False)
         except Error:
             print(f"Error: exited with {cumulative_points}")
-    print(cumulative_points)
+    return cumulative_points
 
-def score_line(them, me):
-    total_points = points[translation[me]]
+def main():
+    print(frame(2))
 
-    if(translation[me] == translation[them]):
-        total_points += points_draw
-    elif(a_beats_b[translation[me]] == translation[them]):
-        total_points += points_win
-    
-    return total_points
-
-part_one()
+main()
