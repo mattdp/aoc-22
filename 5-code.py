aacfea2c7ef922b2f35712e0e7ba97eca7532d4a
Matthew Du Pont <1626189+mattdp@users.noreply.github.com>
2023-05-16 13:14:43 -0400

    working input parser

--- 5-code.py
+++ 5-code.py
@@ -3,14 +3,12 @@
 import re
 from collections import deque
 
-# return ?list of stacks? and a list of move tuples (# crates, from, to)
-def input_parser():
-
-    move_queue = deque()
+# transform stacks and move_queue to have all data
+def input_parser(stacks,move_queue):
 
     f = open("./5-input.txt","r")
     for line in f:
-        if line[0].isspace():
+        if line[0].isspace(): #skip crate stack key, separator
             continue
         elif line[0] == "m": #move line
             matches = re.match("move (\d+) from (\d+) to (\d+)",line)
@@ -18,12 +16,23 @@ def input_parser():
             tup = (a,b,c)
             move_queue.append(tup)
         else: # crate info line
-            #push from top down onto appropriate stack, then map reverse all stacks
-            continue
-
-    return move_queue
+            stack_index = 0
+            char_pos = 1
+            while char_pos <= len(line):
+                if not line[char_pos].isspace():
+                    stacks[stack_index].append(line[char_pos])
+                stack_index += 1
+                char_pos += 4 # "] [" is separator, then 1 more to get to char
+            
+    return stacks, move_queue
 
 def main():
-    print(input_parser())
+
+    stacks = list()
+    for i in range (9):
+        stacks.append(deque())
+    move_queue = deque()
+
+    print(input_parser(stacks,move_queue))
 
 main()
