66e797588e723ac9d86f7b11f00fa80002b81306
Matthew Du Pont <1626189+mattdp@users.noreply.github.com>
2023-05-17 08:40:54 -0400

    setup 7

---
+++ 7-code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+class node:
+
+def part_one(root_node,instruction_list):
+
+def main():
+    f = open("./7-input.txt","r")
+    for line in f:
+        continue 
+
+    root_node = node()    
+    instruction_list = list()
+    
+    part_one(root_node,instruction_list)   
+    
+    print(root_node.total_size())
+
+main()
