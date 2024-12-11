0e2e5a73643b20ead63fc949c05bc7b5c320f3f9
Matt Du Pont <mattdp@users.noreply.github.com>
2024-12-11 14:07:50 -0500

    possible d4p1 solution, check when internet

--- 4-code.py
+++ 4-code.py
@@ -1,48 +1,62 @@
 #!/usr/bin/python3
 
-brainstorm = """
-make into an i,j array
+import re
 
-possibly just transform it over and over - flip, translate
-new array of diagonals
+brainstorm = """
 
 r/l
 u/d
 diag1
 diag2
 
-if read into those 4 things and then regex all of them for xmas it should work
-
-for each X look in all directions
-if near puzzle boundaries don't need to consider checks
+if read into those 4 things and then regex all of them for xmas and smax it should work
 """
 
 def main(part=1):
     f = open("./4-input.txt","r")
     xmases = 0
- 
+    j = 0
+    dimension = 0
+
     ## "we": #left to right
     ## "ns": #up to down
-    ## "swne": #lower left to upper right diagonal
+    ## "nesw": #lower left to upper right diagonal
     ## "nwse": #upper left to lower right diagonal
     x = {}
 
-    j = 0
     for line in f:
         line = line.strip()
+
         if x == {}:
             dimension = len(line)
             x["we"] = [] #different approach since can simply append
             x["ns"] = [""] * dimension
-            print(x["ns"][0])
-            x["swne"] = [[]] * (dimension * 2 - 1) # "rows" triggered by all of one side, n-1 of another side
-            x["nwse"] = [[]] * (dimension * 2 - 1)
+            x["nesw"] = [""] * (dimension * 2 - 1) # "rows" triggered by all of one side, n-1 of another side
+            x["nwse"] = [""] * (dimension * 2 - 1)
         x["we"].append(line)
         for i in range(len(line)):
             x["ns"][i] += line[i]
+            x["nesw"][j+i] += line[i]
+            #aiming to make upper right most "S" its own row
+            #meaning dimension = 140, i = 139, j = 0 should index to 0
+            nwse_index = dimension-(i+1)+j
+            x["nwse"][nwse_index] += line[i]
+            #if(j <= 1):
+            #    print(f"i = {i}, j = {j}, dimension = {dimension}, nwse_index = {nwse_index}")
         j += 1
-    print(x["we"][0])
-    print("break")
-    print(x["ns"][0])
+    #print(x["we"][0])
+    #print(x["ns"][0])
+    #print(x["nesw"])
+    #print(x["nwse"])
+
+    pattern = r"XMAS|SAMX"
+    for key in x.keys():
+        lists = x[key]
+        for l in range(len(lists)):
+            matches = re.findall(pattern,x[key][l])
+            if matches:
+                xmases += len(matches)
+    
+    print(xmases)
 
 main()
